dcde794e928960adc04d4094dda802fb44b378ba
Camila Prado <50000125+milaepg@users.noreply.github.com>
2023-03-28 11:35:59 -0300

    Práctica de Condicionales

---
+++ Introduction/practicas_condiconales.js
@@ -0,0 +1,27 @@
+//1
+var num = 15;
+if (num >= 10) {
+    console.log("el número es mayor que 10");
+}
+//el número es mayor que 10 
+
+//2
+var name = "Bruce";
+if (name != "Bruce") {
+    console.log("¿Cuál es tu nombre?");
+} 
+else {
+    console.log("Hola, Bruce!");
+}
+//Hola, Bruce!
+
+//3
+var num1 = 10;
+var num2 = 15;
+if (num1 <= num2) {
+    num1 = num1 + 10;
+}
+console.log(num1);
+console.log(num2);
+
+//20 - 15 
